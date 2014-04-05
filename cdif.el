12c244b817583f328e3685d936a34be11589d8a8
Kaz Utashiro <kaz@utashiro.com>
2014-04-06 08:47:57 +0900

    do error check in cdif.el

--- cdif.el
+++ cdif.el
@@ -14,12 +14,12 @@
 (defun cdif-region (begin end)
   "Execute cdif command on region and apply ansi-color."
   (interactive "r")
+  (or (executable-find "cdif")
+      (error "cdif command not found"))
   (let ((opoint (point))
 	(modified (buffer-modified-p)))
       (set-mark end)
       (goto-char begin)
-      ;; shell-command-on-region returns exit status 1 for both successful
-      ;; execution and command-not-found failure, and can't tell which.
       (shell-command-on-region begin end "cdif --nocc --no256" t t nil t)
       (message "applying ansi-color. can take a while.")
       (ansi-color-apply-on-region (region-beginning) (region-end))
